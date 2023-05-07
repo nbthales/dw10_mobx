35a7acab026a96612a78b296ccd1ea3e69914da4
nbthales <nbthales@gmail.com>
2023-05-07 12:22:10 -0300

    01:00:54

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Pré Imersão Dart Week - Gerência de estado com MobX
 ## Comandos úteis
 
 `flutter create --project-name dw10_mobx --org br.com.academiadoflutter --platform web ./dw10_mobx`
+`dart run build_runner build -d`
+`dart run build_runner watch -d`
---
+++ lib/home/home_controller.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$HomeController on HomeControllerBase, Store {
+  late final _$nomeAtom =
+      Atom(name: 'HomeControllerBase.nome', context: context);
+
+  @override
+  String get nome {
+    _$nomeAtom.reportRead();
+    return super.nome;
+  }
+
+  @override
+  set nome(String value) {
+    _$nomeAtom.reportWrite(value, super.nome, () {
+      super.nome = value;
+    });
+  }
+
+  @override
+  String toString() {
+    return '''
+nome: ${nome}
+    ''';
+  }
+}
